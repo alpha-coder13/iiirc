0f8554907e4964056e2b02ec087d0bcd4c2e1a41
alpha-coder13 <amardeepsaha13@gmail.com>
2021-11-07 22:19:32 +0530

    new chanes

--- COIN/blockchain.py
+++ COIN/blockchain.py
@@ -2,32 +2,33 @@ import json
 import hashlib
 import random
 from datetime import datetime, time
+from asyncio.tasks import sleep
 import structlog
 import asyncio
+import math
 
 logger=structlog.get_logger()
 
-class blockchain:
+class Blockchain:
 
     def __init__(self):
         self.chain=[]
-        self.transactions=[]
+        self.transactions=[] ## adding transactions to pool is required
         self.target="0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-        print("defining genesis block")
+        logger.info("defining genesis block")
         self.new_block()
     
     def new_block(self):
         block=self.create_block(
             height=len(self.chain),
             Transaction=self.transactions,
-            previous_hash=self.previous_block_hash(self.chain),
+            previous_hash=self.chain[-1]['Hash'] if self.last_block else None ,
             Target=self.target,
-            timestamp=datetime.isoformat(datetime.utcnow),
-            nonce=None
+            timestamp=time.isoformat(time()),
+            nonce="{}".format(random.getrandbits(64))
         )
-
-        self.appendBlock(block)
-        change_target()## editing required
+        
+        return block
 
     @staticmethod
     def create_block(height,Transaction,previous_hash,Target,timestamp,nonce=None):
@@ -35,44 +36,61 @@ class blockchain:
             'Height':height,
             'Timestamp':timestamp,
             'Transactions':Transaction,
-            'Previous_hash':previous_hash, ##doubt
+            'Previous_hash':previous_hash, 
             'Target':Target,
             'Nonce': nonce
         }
-        Block['Hash']=blockchain.hash_block(Block)
+        Block['Hash']=Blockchain.hash_block(Block)
         return Block 
     
     @staticmethod
     def hash_block(Block):
-        return hashlib.sha256(json.dumps(Block).encode('utf-8')).hexdigest()
+        return hashlib.sha256(json.dumps(Block).encode()).hexdigest()
 
-    def set_nonce(self):
+    async def Mine(self):
+        self.change_target(self.chain[-1]['Height']+1 if self.last_block else 1)
+        
         while True:
             new_Block=self.new_block()
             if self.valid(new_Block):
+                logger.info("Nonce Found")
                 break
-        
+            await asyncio.sleep(0)
+            logger.error("Nonce not Found")
+        self.clear_Transaction_pool
         self.chain.append(new_Block)
-        print(self.chain[-1])
-        self.transactions=[]
+        logger.info("New Block found")
 
-    @staticmethod
-    def valid(Block):
-        return Block['Hash'].startswith("0000")
+    @property
+    def clear_Transaction_pool(self): ##clears the trasaction pool
+        self.transactions=[] 
+
+    def valid(self,Block):
+        return int(Block['Hash'],16)<int(self.target,16) ##checks the vaidity of the block
     
     @property
     def last_block(self):
-        if len(self.chain)>0:
-            return self.chain[-1]['Hash']
-        else :
-            return 
+        return len(self.chain)    
     
+    def change_target(self,index):
+        if index%10:
+            return
+        
+        expexted_timespan=10*10
+        
+        actual_timespan=self.chain[-1]['Timestamp']-self.chain[-10]['Timestamp']
 
+        ratio=actual_timespan/expexted_timespan
+        ratio=min(4.00,ratio)
+        ratio=max(0.25,ratio)
 
+        new_target_hash= int(self.target,16)*ratio
+        self.target="{}".format(math.floor(new_target_hash)).zfill(64)
+        
+    def add_transaction():
+        pass
 
-
-
-
-
-
-
+    def add_block(self,Block):
+        #Validation logic method to be added
+        self.clear_Transaction_pool
+        self.chain.append(Block)
---
+++ COIN/teat.py
@@ -0,0 +1,4 @@
+from  math import floor,ceil
+
+target="0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+print(int(target,16))
